4be3a3377c3eaab553d0b63bb50742914c1181b0
codesleep-Beperfect <sagargupta59411@gmail.com>
2023-01-22 15:10:30 +0530

    PB_theme_functions files completed. all basic files function added

--- PB_Task4B_Windows/PB_theme_functions.py
+++ PB_Task4B_Windows/PB_theme_functions.py
@@ -39,7 +39,10 @@ import json
 ##############################################################
 
 ################# ADD UTILITY FUNCTIONS HERE #################
-
+map_str_int={
+    'A':-0.8800,'B':-0.5300,'C':-0.1800,'D':0.1700,'E':0.5350,'F':0.8800,
+    '1':+0.8800,'2':+0.5300,'3':+0.1800,'4':-0.1700,'5':-0.5350,'6':-0.8800
+}
 
 ##############################################################
 
@@ -80,7 +83,9 @@ def setup_server(host, port):
 	server = None
 
 	##################	ADD YOUR CODE HERE	##################
-	
+	server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	server.bind((host,port))
+	server.listen(5)
 
 	##########################################################
 
@@ -109,7 +114,9 @@ def setup_connection(server):
 	address = None
 
 	##################	ADD YOUR CODE HERE	##################
-
+	while connection==None and address==None:
+		print("Trying to connect")
+		connection,address=server.accept()
 
 	##########################################################
 
@@ -139,7 +146,8 @@ def receive_message_via_socket(connection):
 	message = None
 
 	##################	ADD YOUR CODE HERE	##################
-
+	message=connection.recv(1024)
+	message=message.decode("utf-8")
 
 	##########################################################
 
@@ -169,7 +177,7 @@ def send_message_via_socket(connection, message):
 	"""
 
 	##################	ADD YOUR CODE HERE	##################
-
+	connection.send(bytes(message,"utf-8"))
 
 	##########################################################
 
@@ -207,7 +215,13 @@ def read_qr_code(sim):
 	qr_message = None
 	
 	##############  ADD YOUR CODE HERE  ##############
-
+	vision_sensor_handle=sim.getObjectHandle('/Diff_Drive_Bot/vision_sensor')
+	img,shape=sim.getVisionSensorImg(vision_sensor_handle)
+	img=np.frombuffer(img,dtype=np.uint8).reshape(shape[0],shape[1],3)
+	img=cv2.flip(cv2.cvtColor(img,cv2.COLOR_BGR2RGB),0)
+	qrs=decode(img)
+	for qr in qrs:
+		qr_message= qr.data.decode('UTF-8')
 
 	##################################################
 
@@ -248,10 +262,26 @@ def detect_all_nodes(image):
 	end_node = ""
 
     ##############	ADD YOUR CODE HERE	##############
-
+	for i in range(1, 7):
+		for j in range(1, 7):
+			red =image[i*100, j*100][2]
+			if red == 255:
+				loc = chr(64+j)+str(i) 
+				traffic_signals.append(loc)
 
     ##################################################
+	traffic_signals.sort()
+	for i in range(1, 7):
+		for j in range(1, 7):
+			red = image[i*100, j*100][2]
+			green=image[i*100,j*100][1]
 
+			purple=image[i*100,j*100]
+
+			if purple[0]==189 and purple[1]==43 and purple[2]==105:
+				end_node=end_node+(chr(64+j)+str(i))
+			if green==255:
+				start_node=start_node+(chr(64+j)+str(i))
 	return traffic_signals, start_node, end_node
 
 def detect_horizontal_roads_under_construction(image):	
@@ -277,7 +307,22 @@ def detect_horizontal_roads_under_construction(image):
 	horizontal_roads_under_construction = []
 
 	##############	ADD YOUR CODE HERE	##############
-
+	t=2
+    
+	i=6
+	while i>=1:
+		t=2
+		for j in range(1,6):
+			mid_x=int((j*100+t*100)/2)
+			test_img=image[i*100,mid_x-10:mid_x+10]
+			if test_img.all():
+				loc=chr(64+j)+str(i)
+				loc1=chr(64+t)+str(i)
+				loc2=loc+"-"+loc1
+				horizontal_roads_under_construction.append(loc2)
+			t=t+1
+		i=i-1
+	horizontal_roads_under_construction.sort()
 
 	##################################################
 	
@@ -306,7 +351,22 @@ def detect_vertical_roads_under_construction(image):
 	vertical_roads_under_construction = []
 
 	##############	ADD YOUR CODE HERE	##############
-
+	t=2
+	j=1
+	while j<7:
+		t=2
+		for i in range(1,6):
+			mid_y=int((i*100+(t)*100)/2)
+			test_img=image[mid_y,j*100]
+            # print(test_img)
+			if test_img.all():
+				loc=chr(64+j)+str(i)
+				loc1=chr(64+j)+str(t)
+				loc2=loc+"-"+loc1
+				vertical_roads_under_construction.append(loc2)
+                # print((j,i))
+			t=t+1
+		j=j+1
 	
 	##################################################
 	
@@ -344,7 +404,72 @@ def detect_medicine_packages(image):
 	medicine_packages = []
 
 	##############	ADD YOUR CODE HERE	##############
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	img_hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
+	
+	for i in range(1,7):
+		cropped=gray[100:200,i*100:(i+1)*100]
+		color_cr=image[100:200,i*100:(i+1)*100]
+		
+
+
+		_, threshold = cv2.threshold(cropped, 190, 255, cv2.THRESH_BINARY)
+
+		contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		
+		j=0
+		for contour in contours:
+			if j==0:
+				j=j+1
+				continue
+			
+			approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
+		
+
+			cv2.drawContours(cropped, [contour], 0, (0, 0, 255), 5)
+		
+
+			M = cv2.moments(contour)
+			if M['m00'] != 0.0:
+				x = int(M['m10']/M['m00'])
+				y = int(M['m01']/M['m00'])
+
+			
+			j=j+1
+			color=""
+			shape=""
+			shop_number="Shop_"+str(i)
+			if(color_cr[y,x][0]==255 and color_cr[y,x][1]==255 and color_cr[y,x][2]==0):
+				color=color+"Skyblue"
+			elif(color_cr[y,x][0]==0 and color_cr[y,x][1]==127 and color_cr[y,x][2]==255):
+				color=color+"Orange"
+			elif(color_cr[y,x][0]==180 and color_cr[y,x][1]==0 and color_cr[y,x][2]==255):
+				color=color+"Pink"
+			else:
+				color=color+"Green"
+
+				
+
+			if len(approx) == 3:
+				shape=shape+"Triangle"
+
+
+			elif len(approx) == 4:
+				shape=shape+"Square"
+	
+			else:
+				shape=shape+"Circle"
+				
+			
+			local_list=[]
+			local_list.append(shop_number)
+			local_list.append(color)
+			local_list.append(shape)
+			local_list.append([100*i+x,y+100])
+			medicine_packages.append(local_list)
+
+	medicine_packages.sort()
 
 	##################################################
 
@@ -386,7 +511,18 @@ def detect_arena_parameters(maze_image):
 	arena_parameters = {}
 
     ##############	ADD YOUR CODE HERE	##############
-
+	trafic_signals,start,end=detect_all_nodes(maze_image)
+	arena_parameters['traffic_signals']=trafic_signals
+	arena_parameters['start_node']=start
+	arena_parameters['end_node']=end
+	horizontal_roads=detect_horizontal_roads_under_construction(maze_image)
+	arena_parameters['horizontal_roads_under_construction']=horizontal_roads
+	vertical_roads=detect_vertical_roads_under_construction(maze_image)
+	arena_parameters['vertical_roads_under_construction']=vertical_roads
+	medical_packages=detect_medicine_packages(maze_image)
+	arena_parameters['medicine_packages']=medical_packages
+	
+	
 
     ##################################################
 
@@ -443,7 +579,40 @@ def place_packages(medicine_package_details, sim, all_models):
     arena = sim.getObject('/Arena')    
 
 ####################### ADD YOUR CODE HERE #########################
-
+    for shop,color,shape,centroid in medicine_package_details:
+        shop_no=int(shop[-1])
+        # print(shop_no)
+        x,y=centroid[0]-100*shop_no,centroid[1]-100
+        packet=0
+        if x==30 and y==30:
+            packet=1
+        if x==70 and y==30:
+            packet=2
+        if x==30 and y==70:
+            packet=3
+        if x==70 and y==70:
+            packet=4
+        packet=packet-1
+        x_coor=-(0.8800-0.36*(shop_no-1)-0.04*(2*packet+1)-0.01+0.005)
+        # print(packet)
+        y_coor=+(0.6650)
+        shape_3d=''
+        if shape=='Square':
+            shape_3d='_cube'
+        if shape =='Triangle':
+            shape_3d='_cone'
+        if shape=='Circle':
+            shape_3d='_cylinder'
+        
+        path_file=color+shape_3d+".ttm"
+        temp_path=os.path.join(packages_models_directory,path_file)
+        # print(temp_path)
+        # print(temp_path)
+        t=sim.loadModel(temp_path)
+        sim.setObjectParent(t,arena)
+        sim.setObjectAlias(t,color+shape_3d)
+        sim.setObjectPosition(t,arena,(x_coor,y_coor,0.003))
+        all_models.append(t)
 
 
 ####################################################################
@@ -486,7 +655,18 @@ def place_traffic_signals(traffic_signals, sim, all_models):
     arena = sim.getObject('/Arena')
 
 ####################### ADD YOUR CODE HERE #########################
+    print(traffic_signals)
+    for node in traffic_signals:
+        t=sim.loadModel(traffic_sig_model)
+        # print(t)
 
+        sim.setObjectParent(t,arena)
+        sim.setObjectAlias(t,"Signal_"+node)
+        # node_list=list(node)
+        x=map_str_int[node[0]]
+        y=map_str_int[node[1]]
+        sim.setObjectPosition(t,arena,(x,y,0.15588))
+        all_models.append(t)
 
 ####################################################################
 
@@ -531,7 +711,24 @@ def place_start_end_nodes(start_node, end_node, sim, all_models):
     arena = sim.getObject('/Arena')   
 
 ####################### ADD YOUR CODE HERE #########################
-
+    t=sim.loadModel(start_node_model)
+    # print(t)
+    sim.setObjectParent(t,arena)
+    sim.setObjectAlias(t,"Start_Node")
+        # node_list=list(node)
+    x=map_str_int[start_node[0]]
+    y=map_str_int[start_node[1]]
+    sim.setObjectPosition(t,arena,(x,y,0.15588))
+    all_models.append(t)
+    t=sim.loadModel(end_node_model)
+    # print(t)
+    sim.setObjectParent(t,arena)
+    sim.setObjectAlias(t,"End_Node")
+        # node_list=list(node)
+    x=map_str_int[end_node[0]]
+    y=map_str_int[end_node[1]]
+    sim.setObjectPosition(t,arena,(x,y,0.15588))
+    all_models.append(t)
 
 ####################################################################
 
@@ -575,7 +772,16 @@ def place_horizontal_barricade(horizontal_roads_under_construction, sim, all_mod
     arena = sim.getObject('/Arena')  
 
 ####################### ADD YOUR CODE HERE #########################
-
+    for roads in horizontal_roads_under_construction:
+        t=sim.loadModel(horiz_barricade_model)
+        sim.setObjectParent(t,arena)
+        sim.setObjectAlias(t,"Horizontal_missing_road_"+roads[0]+roads[1]+"_"+roads[3]+roads[4])
+        x1,y1=map_str_int[roads[0]],map_str_int[roads[1]]
+        x2,y2=map_str_int[roads[3]],map_str_int[roads[4]]
+        x3=(x1+x2)/2
+        y3=(y1+y2)/2
+        sim.setObjectPosition(t,arena,(x3,y3,0.003))
+        all_models.append(t)
 
 ####################################################################
 
@@ -620,7 +826,16 @@ def place_vertical_barricade(vertical_roads_under_construction, sim, all_models)
     arena = sim.getObject('/Arena')
 
 ####################### ADD YOUR CODE HERE #########################
-
+    for roads in vertical_roads_under_construction:
+        t=sim.loadModel(vert_barricade_model)
+        sim.setObjectParent(t,arena)
+        sim.setObjectAlias(t,"Vertical_missing_road_"+roads[0]+roads[1]+"_"+roads[3]+roads[4])
+        x1,y1=map_str_int[roads[0]],map_str_int[roads[1]]
+        x2,y2=map_str_int[roads[3]],map_str_int[roads[4]]
+        x3=(x1+x2)/2
+        y3=(y1+y2)/2
+        sim.setObjectPosition(t,arena,(x3,y3,0.003))
+        all_models.append(t)
 
 ####################################################################
 
