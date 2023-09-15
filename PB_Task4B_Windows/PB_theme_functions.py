53443d17e469c550ab925270d2af5dd5649daf5f
Saksham Gatuam <88222914+madmaverickminion@users.noreply.github.com>
2023-09-16 01:35:54 +0530

    Add files via upload

--- PB_Task4B_Windows/PB_theme_functions.py
+++ PB_Task4B_Windows/PB_theme_functions.py
@@ -36,6 +36,8 @@ import numpy as np
 import cv2
 from pyzbar.pyzbar import decode
 import json
+import math
+from cv2 import aruco
 ##############################################################
 
 ################# ADD UTILITY FUNCTIONS HERE #################
@@ -146,7 +148,7 @@ def receive_message_via_socket(connection):
 	message = None
 
 	##################	ADD YOUR CODE HERE	##################
-	message=connection.recv(1024)
+	message=connection.recv(80)
 	message=message.decode("utf-8")
 
 	##########################################################
@@ -842,4 +844,345 @@ def place_vertical_barricade(vertical_roads_under_construction, sim, all_models)
     return all_models
 
 ##############################################################
+def detect_ArUco_details(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns a dictionary such
+    that the id of the ArUco marker is the key and a list of details of the marker
+    is the value for each item in the dictionary. The list of details include the following
+    parameters as the items in the given order
+        [center co-ordinates, angle from the vertical, list of corner co-ordinates] 
+    This order should be strictly maintained in the output
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by cv2 library
+    Returns:
+    ---
+    `ArUco_details_dict` : { dictionary }
+            dictionary containing the details regarding the ArUco marker
+    
+    Example call:
+    ---
+    ArUco_details_dict = detect_ArUco_details(image)
+    """    
+
+    ##############	ADD YOUR CODE HERE	##############
+    ArUco_details_dict = {} 
+    ArUco_corners = {}
+    ARUCO_DICT = {
+        "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+        "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+        "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+        "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+        "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+        "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+        "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+        "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+        "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+    }
+
+    for (arucoName, arucoDict) in ARUCO_DICT.items():
+        arucoDict = cv2.aruco.Dictionary_get(arucoDict)
+        arucoParams = cv2.aruco.DetectorParameters_create()
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+        # if at least one ArUco marker was detected display the ArUco
+        # name to our terminal
+        
+        if len(corners) > 0:
+            i=0
+
+            for id in ids:
+                
+                ArUco_corners[id[0]]=corners[i][0]
+                i=i+1
+            
+             
+    
+    for (id,points) in ArUco_corners.items():
+        # print(points)
+        point_1=points[0]
+        point_2=points[1]
+        point_3=points[2]
+        aruco_mid_x=round((point_1[0]+point_3[0])/2)
+        aruco_mid_y=round((point_1[1]+point_3[1])/2)
+
+        mid_x_1_2=(point_1[0]+point_2[0])/2
+        mid_y_1_2=(point_1[1]+point_2[1])/2
+        
+        shifted_x=mid_x_1_2-aruco_mid_x
+        shifted_y=-mid_y_1_2+aruco_mid_y
+        # print(-int(math.atan2(shifted_x,shifted_y)*57.29))
+        ArUco_details_dict[int(id)]=[[aruco_mid_x,aruco_mid_y],-round(math.atan2(shifted_x,shifted_y)*180/3.141592653589793)]
+    ##################################################
+    
+    return ArUco_details_dict, ArUco_corners 
+
 ##############################################################
+
+def detect_paths_to_graph(image):
+	"""
+	Purpose:
+	---
+	This function takes the image as an argument and returns a dictionary of the
+	connect path from a node to other nodes and will be used for path planning
+
+	HINT: Check for the road besides the nodes for connectivity 
+
+	Input Arguments:
+	---
+	`maze_image` :	[ numpy array ]
+			numpy array of image returned by cv2 library
+	Returns:
+	---
+	`paths` : { dictionary }
+			Every node's connection to other node and set it's value as edge value 
+			Eg. : { "D3":{"C3":1, "E3":1, "D2":1, "D4":1}, 
+					"D5":{"C5":1, "D2":1, "D6":1 }  }
+
+			Why edge value 1? -->> since every road is equal
+
+	Example call:
+	---
+	paths = detect_paths_to_graph(maze_image)
+	"""    
+
+	paths = {}
+
+	##############	ADD YOUR CODE HERE	##############
+	for i in range(1,7):
+		for j in range(1,7):
+			point={}
+				#i+1,j
+				#i-1,j
+				#i,j+1
+				#i,j-1
+			
+			if i>=1 and i<=6 and j+1>=1 and j+1<=6:
+				mid_x=int(((j+1)*100+j*100)/2)
+				test_img=image[i*100,mid_x-10:mid_x+10]
+				if test_img.all()==0:
+					point[""+(chr(64+j+1)+str(i))]=1
+				
+			if i>=1 and i<=6 and j-1>=1 and j-1<=6:
+				mid_x=int(((j-1)*100+j*100)/2)
+				test_img=image[i*100,mid_x-10:mid_x+10]
+				if test_img.all()==0:
+					point[""+(chr(64+j-1)+str(i))]=1
+
+			if i+1>=1 and i+1<=6 and j>=1 and j<=6:
+				mid_y=int(((i+1)*100+i*100)/2)
+				test_img=image[mid_y,j*100]
+				if test_img.all()==0:
+					point[""+(chr(64+j)+str(i+1))]=1
+			if i-1>=1 and i-1<=6 and j>=1 and j<=6:
+				mid_y=int(((i-1)*100+i*100)/2)
+				test_img=image[mid_y,j*100]
+				if test_img.all()==0:
+					point[""+(chr(64+j)+str(i-1))]=1
+				
+			# sorted(point)
+			point_sorted={}
+			for t in sorted(point):
+				point_sorted[t]=point[t]
+				
+
+			paths[""+(chr(64+j)+str(i))]=point_sorted
+        
+
+	##################################################
+	paths_sorted={}
+	for i in sorted(paths):
+		paths_sorted[i]=paths[i]
+	# print(paths_sorted)
+	return paths_sorted
+def find_minimum(node,vis,dist):
+    min_dis=100000000
+    curr_nod=""
+    # print(vis)
+    for i in vis:
+        # print(i)
+        if(vis[i]==0):
+			# #### change here
+           if dist[i]<min_dis:
+            min_dis=dist[i]
+            # curr_node=""
+            curr_nod=i
+    return curr_nod
+
+def path_planning(graph, start, end):
+	# print(graph)
+	"""
+	Purpose:
+	---
+	This function takes the graph(dict), start and end node for planning the shortest path
+
+	** Note: You can use any path planning algorithm for this but need to produce the path in the form of 
+	list given below **
+
+	Input Arguments:
+	---
+	`graph` :	{ dictionary }
+			dict of all connecting path
+	`start` :	str
+			name of start node
+	`end` :		str
+			name of end node
+
+
+	Returns:
+	---
+	`backtrace_path` : [ list of nodes ]
+			list of nodes, produced using path planning algorithm
+
+		eg.: ['C6', 'C5', 'B5', 'B4', 'B3']
+	
+	Example call:
+	---
+	arena_parameters = detect_arena_parameters(maze_image)
+	"""    
+
+	backtrace_path=[]
+
+	##############	ADD YOUR CODE HERE	##############
+	node=36
+	dist={}
+	for i in graph:
+		dist[i]=1e9
+	vis={}
+	path={}
+	for i in graph:
+		vis[i]=0
+		path[i]=""
+		dist[start]=0
+	for i in range(node):
+		# find  minimum  node
+		min_node=find_minimum(node,vis,dist)
+		# print(min_node)
+		vis[min_node]=1
+		if min_node=="":
+			continue 
+		for j in graph[min_node]:
+			# print(graph[min_node][j])
+			# print(j)
+			if(graph[min_node][j]+dist[min_node]<dist[j]):
+				dist[j]=graph[min_node][j]+dist[min_node]
+				path[j]=min_node
+	curr_node=end
+	backtrace_path=[]
+	while True:
+		if curr_node==start:
+			break
+		backtrace_path.append(curr_node)
+		curr_node=path[curr_node]
+
+	backtrace_path.append(start)
+	backtrace_path.reverse()
+	##################################################
+
+	# print(backtrace_path)
+	return backtrace_path
+robot_direction="NORTH"
+def paths_to_moves(paths, traffic_signal):
+	global robot_direction
+	# print(traffic_signal)																					
+	"""
+	Purpose:
+	---
+	This function takes the list of all nodes produces from the path planning algorithm
+	and connecting both start and end nodes
+
+	Input Arguments:
+	---
+	`paths` :	[ list of all nodes ]
+			list of all nodes connecting both start and end nodes (SHORTEST PATH)
+	`traffic_signal` : [ list of all traffic signals ]
+			list of all traffic signals
+	---
+	`moves` : [ list of moves from start to end nodes ]
+			list containing moves for the bot to move from start to end
+
+			Eg. : ['UP', 'LEFT', 'UP', 'UP', 'RIGHT', 'DOWN']
+	
+	Example call:
+	---
+	moves = paths_to_moves(paths, traffic_signal)
+	"""    
+	
+	list_moves=[]
+
+	##############	ADD YOUR CODE HERE	##############
+	
+	for i in range(len(paths)-1):
+		node_curr=paths[i]
+		node_next=paths[i+1]
+		if node_curr in traffic_signal:
+			list_moves.append("WAIT_5")
+		# print(node_curr[0])
+		if node_curr[0]==node_next[0]:
+			if node_next[1]<node_curr[1]:
+				if robot_direction=="NORTH":
+					list_moves.append("STRAIGHT")
+					robot_direction="NORTH"
+				elif robot_direction=="WEST":
+					list_moves.append("RIGHT")
+					robot_direction="NORTH"
+				elif robot_direction=="EAST":
+					list_moves.append("LEFT")
+					robot_direction="NORTH"
+					
+				elif robot_direction=="SOUTH":
+					print("Line no.1136")
+					list_moves.append("REVERSE")
+					robot_direction="NORTH"
+			elif node_next[1]>node_curr[1]:
+				if robot_direction=="NORTH":
+					print("Line no.1141")
+					list_moves.append("REVERSE")
+					robot_direction="SOUTH"
+					
+				elif robot_direction=="WEST":
+					list_moves.append("LEFT")
+					robot_direction="SOUTH"
+				elif robot_direction=="EAST":
+					list_moves.append("RIGHT")
+					robot_direction="SOUTH"
+				elif robot_direction=="SOUTH":
+					list_moves.append("STRAIGHT")
+					robot_direction="SOUTH"
+		else:
+			if node_next[0]<node_curr[0]:
+				if robot_direction=="NORTH":
+					list_moves.append("LEFT")
+					robot_direction="WEST"
+				elif robot_direction=="WEST":
+					list_moves.append("STRAIGHT")
+					robot_direction="WEST"
+				elif robot_direction=="EAST":
+					print("Line no.1163")
+					list_moves.append("REVERSE")
+					robot_direction="WEST"
+				elif robot_direction=="SOUTH":
+					list_moves.append("RIGHT")
+					robot_direction="WEST"
+			else :
+				if robot_direction=="NORTH":
+					list_moves.append("RIGHT")
+					robot_direction="EAST"
+				elif robot_direction=="WEST":
+					print("Line no.1174")
+					list_moves.append("REVERSE")
+					robot_direction="EAST"
+				elif robot_direction=="EAST":
+					list_moves.append("STRAIGHT")
+					robot_direction="EAST"
+				elif robot_direction=="SOUTH":
+					list_moves.append("LEFT")
+					robot_direction="EAST"
+
+	##################################################
+
+	return list_moves
--- PB_Task4B_Windows/task_4b.py
+++ PB_Task4B_Windows/task_4b.py
@@ -1,3 +1,7 @@
+#reverse vali condition 
+#bot slow
+#define 4 values of nodes min and max
+#camera fps increase using multithreading 
 '''
 *****************************************************************************************
 *
@@ -14,7 +18,17 @@
 *
 *****************************************************************************************
 '''
-
+##Problems that we are facing
+'''
+frame freezes as soon as node is detected
+input message not receiving in correct format. 
+expected -: 
+Node 
+Node 
+NONODE
+Message Received-:
+NodeNodeNONODE
+'''
 # Team ID:			[ Team-ID ]
 # Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
 # Filename:			task_4b.py
@@ -32,11 +46,15 @@ import os, sys
 from zmqRemoteApi import RemoteAPIClient
 import traceback
 import zmq
-import numpy as np
+import numpy
 import cv2
 from pyzbar.pyzbar import decode
 import json
 import random
+from threading import Thread
+import threading
+import math	
+from cv2 import aruco
 ##############################################################
 
 ## Import PB_theme_functions code
@@ -54,7 +72,223 @@ except Exception as e:
 	traceback.print_exc(file=sys.stdout)
 	sys.exit()
 
-def task_4b_implementation(sim):
+
+###############################################******************************************************************************************
+x1=-10
+y1=-10
+x2=-10
+y2=-10
+x3=-10
+y3=-10
+x4=-10
+y4=-10
+tl=()
+tr=()
+bl=()
+br=()
+def perspective_transform(image):
+    global x1,y1,x2,y2,x3,y3,x4,y4,tl,tr,bl,br
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns the image after 
+    applying perspective transform on it. Using this function, you should
+    crop out the arena from the full frame you are receiving from the 
+    overhead camera feed.
+
+    HINT:
+    Use the ArUco markers placed on four corner points of the arena in order
+    to crop out the required portion of the image.
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by cv2 library 
+
+    Returns:
+    ---
+    `warped_image` : [ numpy array ]
+            return cropped arena image as a numpy array
+    
+    Example call:
+    ---
+    warped_image = perspective_transform(image)
+    """   
+    warped_image = []
+    Aruco_details,Aruco_corners=pb_theme.detect_ArUco_details(image) 
+    if(x1==-10 and y1==-10):
+        if Aruco_details.get(1):
+            #br
+            x1=Aruco_details[1][0][0]
+            y1=Aruco_details[1][0][1]
+            tl=(x1,y1)
+    if(x2==-10 and y2==-10):
+        if Aruco_details.get(2):
+            #bl
+            x2=Aruco_details[2][0][0]
+            y2=Aruco_details[2][0][1]
+            tr=(x2,y2)
+    if(x3==-10 and y3==-10):
+        if Aruco_details.get(3):
+            #tl
+            x3=Aruco_details[3][0][0]
+            y3=Aruco_details[3][0][1]
+            br=(x3,y3)
+    if(x4==-10 and y4==-10):
+        
+        if Aruco_details.get(4):
+            #tr
+            x4=Aruco_details[4][0][0]
+            y4=Aruco_details[4][0][1]  
+            bl=(x4,y4)  
+    if x1!=-10 and x2!=-10 and x3!=-10 and x4!=-10:
+        # print(Aruco_details[1][0])
+        pts1=numpy.float32([tl,bl,tr,br])
+        pts2=numpy.float32([[0,0],[0,440],[410,0],[410,440]])
+        matrix=cv2.getPerspectiveTransform(pts1,pts2)
+        warped_image=cv2.warpPerspective(image,matrix,(410,440))
+        # cv2.imshow('transformed_frame',transformed_frame)
+
+    return warped_image
+
+def transform_values(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns the 
+    position and orientation of the ArUco marker (with id 5), in the 
+    CoppeliaSim scene.
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by camera
+
+    Returns:
+    ---
+    `scene_parameters` : [ list ]
+            a list containing the position and orientation of ArUco 5
+            scene_parameters = [c_x, c_y, c_angle] where
+            c_x is the transformed x co-ordinate [float]
+            c_y is the transformed y co-ordinate [float]
+            c_angle is the transformed angle [angle]
+    
+    HINT:
+        Initially the image should be cropped using perspective transform 
+        and then values of ArUco (5) should be transformed to CoppeliaSim
+        scale.
+    
+    Example call:
+    ---
+    scene_parameters = transform_values(image)
+    """   
+    scene_parameters = []
+
+    Aruco_details1,Aruco_corners1=pb_theme.detect_ArUco_details(image)
+    # task_1b.mark_ArUco_image(frame,Aruco_details1, Aruco_corners1)
+    # cv2.imshow('marked',frame)
+    # print(Aruco_details)
+    if Aruco_details1.get(5):
+        x_pixel=Aruco_details1[5][0][0]
+        y_pixel=Aruco_details1[5][0][1]
+        # print(x_pixel,y_pixel)
+        y_cop=0.0044*y_pixel-0.9787+0.03
+        pixel_per_cm=103.85*y_cop*y_cop*y_cop*y_cop*y_cop-5.0625*y_cop*y_cop*y_cop*y_cop-81.685*y_cop*y_cop*y_cop-4.182*y_cop*y_cop+9.8966*y_cop+210.86
+        x_cop=(215-x_pixel)*1.0/pixel_per_cm-0.05
+        # print(x_cop,y_cop)
+
+        angle_image=Aruco_details1[5][1]
+        # angle_cop=0
+        # if angle_image>0:
+        #     angle_cop=180-angle_image
+        # if angle_image<=0:
+        #     angle_cop=angle_image+180
+        # print(angle_image,angle_cop)
+        scene_parameters.append(x_cop)
+        scene_parameters.append(y_cop)
+        scene_parameters.append(angle_image)
+    return scene_parameters
+
+
+def set_values(scene_parameters):
+    """
+    Purpose:
+    ---
+    This function takes the scene_parameters, i.e. the transformed values for
+    position and orientation of the ArUco marker, and sets the position and 
+    orientation in the CoppeliaSim scene.
+
+    Input Arguments:
+    ---
+    `scene_parameters` :	[ list ]
+            list of co-ordinates and orientation obtained from transform_values()
+            function
+
+    Returns:
+    ---
+    None
+
+    HINT:
+        Refer Regular API References of CoppeliaSim to find out functions that can
+        set the position and orientation of an object.
+    
+    Example call:
+    ---
+    set_values(scene_parameters)
+    """   
+    
+    aruco_handle = sim.getObject('/aruco_5')
+    aruco_handle_3=sim.getObject('/Arena')
+
+    if len(scene_parameters):
+        sim.setObjectPosition(aruco_handle,aruco_handle_3,[scene_parameters[0],scene_parameters[1],0])
+        # if( scene_parameters[2]<=90):
+        #     alpha=(90-scene_parameters[2])*0.0175
+        # else :
+        #     alpha=(scene_parameters[2]-90)*0.0175
+        # print(scene_parameters[2])
+        angle_rad=scene_parameters[2]*0.0175
+        sim.setObjectOrientation(aruco_handle,sim.handle_world,[0,0,angle_rad])
+
+    return None
+
+########################################################################*****************************************************************
+
+			
+def code_moves(path_to_moves):
+	final=""
+	for move in path_to_moves:
+		if move=="STRAIGHT":
+			final=final+"S_"
+		elif move=="LEFT":
+			final=final+"L_"
+		elif move=="RIGHT":
+			final=final+"R_"
+		elif move=="REVERSE":
+			final=final+"RV_"
+		elif move=="WAIT_5":
+			final=final+"W5_"
+	return final
+
+def target_reached(a_x,a_y):
+	#x_min_e4 y_max_e4 y_min_e4 y_max_e4
+	x_min_e4=0.5192
+	x_max_e4=0.6592
+	y_max_e4=-0.04809
+	y_min_e4=-0.0900
+
+	#x_min_e5 y_max_e5 y_min_e5 y_max_e5
+	x_min_e5=0.5062
+	x_max_e5=0.6662
+	y_min_e5=-0.5095
+	y_max_e5=-0.3495
+	if a_x>=x_min_e4 and a_x<=x_max_e4 and a_y>=y_min_e4 and a_y<=y_max_e4:
+		return True
+	if a_x>=x_min_e5 and a_x<=x_max_e5 and a_y>=y_min_e5 and a_y<=y_max_e5:
+		return True
+	return False
+def task_4b_implementation(sim,config_image,start_node,end_node,traffic_signals,connection):
 	"""
 	Purpose:
 	---
@@ -77,9 +311,104 @@ def task_4b_implementation(sim):
 	"""
 
 	##################	ADD YOUR CODE HERE	##################
-	
+	x_min=0.7871
+	x_max=0.9472
+	y_min=0.4261
+	y_max=0.5361
+	target_nodes=[start_node,"F2",end_node]
+	graph=pb_theme.detect_paths_to_graph(config_image)
+	i=1
+	backtrace_path=pb_theme.path_planning(graph,start_node,"F2")
+	path_to_moves=pb_theme.paths_to_moves(backtrace_path,traffic_signals)
+	moves_code=code_moves(path_to_moves)
+	pb_theme.send_message_via_socket(connection,moves_code)
 
-	##########################################################
+	cap = cv2.VideoCapture(0)
+
+	counter=0
+	counterf2=0
+	while True:
+		ret, frame = cap.read()
+		cv2.imshow('frame',frame)
+		if cv2.waitKey(100) & 0xFF == ord('q'):
+			break
+		frame=frame[40:,100:510]
+		
+		cameraMatrix=numpy.array([[669.2721006 ,   0.    ,     308.27537125],
+[  0.      ,   664.83129387 ,268.35372325],
+[  0.,           0.  ,         1.  ,      ]])
+		dist=numpy.array([[-7.77722198e-01 , 2.17364635e-02, -7.01392560e-04 , 5.27929722e-03,
+1.30080094e+00]])
+		h,  w = frame.shape[:2]
+		newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
+		dst = cv2.undistort(frame, cameraMatrix, dist, None, newCameraMatrix)
+		t=perspective_transform(dst)
+
+		if len(t):
+			
+			a=transform_values(t)
+			#print(a)
+			set_values(a)
+		#check only for f2 predefine four values xmin xmax, ymin ymax
+		#counter=1 when f2 is detected#inside f2 block
+		#decrypt message in f2  condition#inside f2 block 
+		#update target node according to decrypted message# inside f2 block
+		#check value of counter - if counter is one -
+		#											startnode= target[i] endnode=target[i+1] increment value of i 
+		#											pathplanning and backtrace path
+		#											send message to raspi
+		#											if i == len(target nodes)-1 :break
+			if a:
+				a_x=a[0]
+				a_y=a[1]
+				if x_min<=a_x and x_max>=a_x and y_min<=a_y and y_max>=a_y and counterf2==0:
+					counter = 1 
+					counterf2=1
+					# message decryption 
+					print("Line no 370")
+					copp_message={ "Pink_Cone":"E4","Orange_Cone":"E5"}
+					for node in copp_message.values():
+						target_nodes.insert(-1,node)
+					print(target_nodes)
+					colour_string=""
+					for colour in copp_message:
+						colour=colour.split("_")
+						colour_string=colour_string+colour[0]+"_"
+					
+					pb_theme.send_message_via_socket(connection,colour_string)
+
+				# target_reached(ax,ay) return True for reached and false for not reached -:
+				#																			if reached counter=1
+				if target_reached(a_x,a_y):
+					print("Line no. 377")
+					counter=1
+
+
+				if counter:
+					print("Line no. 382")
+					counter=0
+					start=target_nodes[i]
+					end=target_nodes[i+1]
+					print("start_node ",start)
+					print("End node ",end)
+					
+					i=i+1
+					backtrace_path=pb_theme.path_planning(graph,start,end)
+					path_to_moves=pb_theme.paths_to_moves(backtrace_path,traffic_signals)
+					print(backtrace_path)
+					moves_code=code_moves(path_to_moves)
+					print(path_to_moves)
+					pb_theme.send_message_via_socket(connection,moves_code)
+					if i==len(target_nodes)-1:
+						print("Line no. 390")
+						break
+				
+
+
+				##########################################################
+	
+	cap.release()
+	cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
@@ -103,6 +432,7 @@ if __name__ == "__main__":
 
 	## Set up new connection with a socket client (PB_task3d_socket.exe)
 	try:
+		pass
 		print("\nPlease run PB_socket.exe program to connect to PB_socket client")
 		connection_1, address_1 = pb_theme.setup_connection(server)
 		print("Connected to: " + address_1[0] + ":" + str(address_1[1]))
@@ -141,7 +471,7 @@ if __name__ == "__main__":
 		end_node = detected_arena_parameters['end_node']
 		horizontal_roads_under_construction = detected_arena_parameters['horizontal_roads_under_construction']
 		vertical_roads_under_construction = detected_arena_parameters['vertical_roads_under_construction']
-
+		
 		# print("Medicine Packages: ", medicine_package_details)
 		# print("Traffic Signals: ", traffic_signals)
 		# print("Start Node: ", start_node)
@@ -217,8 +547,8 @@ if __name__ == "__main__":
 	## Send Start Command to Raspberry Pi to start execution
 	pb_theme.send_message_via_socket(connection_2, "START")
 
-	
-	task_4b_implementation(sim)
+	#### pass config_image in this function task_4b_implementation(sim,config_image,start_node,end_node,traffic_signals,connection
+	task_4b_implementation(sim,config_img,start_node,end_node,traffic_signals,connection_2)
 
 	## Send Stop Simulation Command to PB_Socket
 	pb_theme.send_message_via_socket(connection_1, "SIMULATION_STOP")
